d0e9356b528ba6bfde37eaef854aa5346a0a275e
usenanayake <usenana94@gmail.com>
2017-09-12 19:00:20 +0530

    changed side bars

--- hotel_teamlead/image.php
+++ hotel_teamlead/image.php
@@ -66,45 +66,18 @@ if(isset($_POST['btnsave'])){
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="index.html">Dashboard</a></li>
-                      <li><a href="index2.html">Dashboard2</a></li>
-                      <li><a href="index3.html">Dashboard3</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-edit"></i> Committee <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="form.html">Publicity Group</a></li>
-                      <li><a href="form_advanced.html">Sponsorship Hndling Group</a></li>
-                      <li><a href="form_validation.html">Bag Quatation Group</a></li>
-                      <li><a href="form_wizards.html">Food Allocation Group</a></li>
-                      <li><a href="form_upload.html">Paper Handling Group</a></li>
-                      <li><a href="form_buttons.html">Keynote Group</a></li>
-                      <li><a href="#">Hotel Quataion Group</a></li>   
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-desktop"></i> Assiging Task <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="general_elements.html">General Elements</a></li>
-                      <li><a href="media_gallery.html">Media Gallery</a></li>
-                      <li><a href="typography.html">Typography</a></li>
-                      <li><a href="icons.html">Icons</a></li>
-                      <li><a href="glyphicons.html">Glyphicons</a></li>
-                      <li><a href="widgets.html">Widgets</a></li>
-                      <li><a href="invoice.html">Invoice</a></li>
-                      <li><a href="inbox.html">Inbox</a></li>
-                      <li><a href="calendar.html">Calendar</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-table"></i> Committee Details <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="tables.html">Tables</a></li>
-                      <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                    </ul>
+                  <li><a href="hleader.php"><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                    
                   </li>
                   
+                  <li><a href="hmembers.php"><i class="fa fa-edit"></i> Committee Details <span class="fa fa-chevron-down"></span></a>
                   
+                </li>
+                  <li><a href="msaintask.php"><i class="fa fa-desktop"></i> Assigning Task <span class="fa fa-chevron-down"></span></a>
+                    
+                  </li>
+                  <li><a href="hotelm.php"><i class="fa fa-table"></i> Committee Database<span class="fa fa-chevron-down"></span></a>
+                   
                 </ul>
               </div>
               
