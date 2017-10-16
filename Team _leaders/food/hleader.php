5581d0d9527908da70bdeb7bc0d65a24e06900b1
Unknown <pathum9868@gmail.com>
2017-10-16 22:00:58 +0530

    change the profile name

--- Team _leaders/food/hleader.php
+++ Team _leaders/food/hleader.php
@@ -76,8 +76,8 @@ mysqli_close($conn);
                 <img src="images/img.jpg" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
-                <span>Welcome,</span>
-                <h2>Admin Doe</h2> </div> </div> <!-- /menu profile quick info -->
+                <span>Welcome</span>
+                <h2>Admin</h2> </div> </div> <!-- /menu profile quick info -->
             <br />
 
             <!-- sidebar menu -->
