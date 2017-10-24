263e0a4bc6d89d7ba3f66ebc2bf751c177323781
LGCPrabuddha <charakacs13@gmail.com>
2017-10-24 12:39:17 +0530

    sponsorship committee member progress view update

--- my/teamlead/production/member/sponsorship/myTask.php
+++ my/teamlead/production/member/sponsorship/myTask.php
@@ -6,6 +6,29 @@
   $task_list1 = '';
   /*$m_index1 = */
   /*$m_index = $_SESSION['user_name']; index eka gana thawa tikak balanna */
+
+  $sql="SELECT * FROM tsponsorship,login WHERE login.user_name = '{$_SESSION['user_name']}' AND login.uindex = tsponsorship.uindex AND tsponsorship.is_deleted = 0";
+
+  if ($result=mysqli_query($connection,$sql))
+    {
+    // Return the number of rows in result set
+    $rowcount=mysqli_num_rows($result);
+    $sql2="SELECT * FROM tpublicity,login WHERE tsponsorship.done='Finish' AND login.user_name = '{$_SESSION['user_name']}' AND login.uindex = tsponsorship.uindex AND tsponsorship.is_deleted = 0 ";
+
+    if($query2=mysqli_query($connection,$sql2)){
+      $result2=mysqli_num_rows($query2);
+    }
+    //Create the Percentage
+    function percentage($a, $b){
+      return ($a/$b)*100;
+    }
+    $percent=percentage($result2,$rowcount);
+    
+    // Free result set
+    mysqli_free_result($result);
+    mysqli_free_result($query2);
+    }
+
   $query = "SELECT * FROM tsponsorship,login WHERE login.user_name = '{$_SESSION['user_name']}' AND login.uindex = tsponsorship.uindex AND tsponsorship.is_deleted = 0   ORDER BY tsponsorship.id";
   $tasks = mysqli_query($connection,$query);
 
@@ -62,6 +85,13 @@
 
               <div style="width: 1000px;" class="process" id="process-heading">
                 <h3>My Task </h3>
+
+                <br>
+                <h3 style="color: #708090;"> Progess of My Task </h3>
+                
+                <div class="progress">
+                   <div class="progress-bar progress-bar-striped" role="progressbar" style="width: <?php echo $percent; ?>%; height: 20px;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><?php echo $percent; ?>%</div>
+                </div>
 
                 <main>
                   <table class="mhTable">
