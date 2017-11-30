22923335d7fcc3203d4b507aad27f76e4415a49c
Unknown <pathum9868@gmail.com>
2017-11-30 20:37:34 +0530

    ui mistake corrected

--- Team _leaders/review/msaintask.php
+++ Team _leaders/review/msaintask.php
@@ -37,6 +37,9 @@ $result=mysqli_query($connection,"SELECT * FROM trial");
     
 
     <title>User Interface </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
     <!-- Our customize file -->
     <link href="css/CDetail.css" rel="stylesheet">
     <!-- Bootstrap -->
