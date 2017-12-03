3644d37a0416bb55cde2305c068b75e3d1e68c25
usenanayake <usenana94@gmail.com>
2017-12-04 00:41:59 +0530

    leadeer assign interface

--- Team _leaders/review by pathum/leader_assign1.php
+++ Team _leaders/review by pathum/leader_assign1.php
@@ -25,32 +25,7 @@
     <!-- Custom Theme Style -->
     <link href="../build/css/custom.min.css" rel="stylesheet">
     <style type="text/css">
-   /* .login_wrapper{
-      margin-top: 0px;
-    }
-    .man{
-      padding-bottom:70%;
-
-    } 
-    .footerTab {
-width: 100%; background-color: black; height: 100px; margin-top: 1000px;
-      }*/
-
-/*div.relative {
-    position: relative;
-    width: 400px;
-    height: 200px;
-    border: 3px solid #8AC007;
-}*/
-
-/*div.absolute {
-    position: absolute;
-    margin-top:  70px;
-    width: 200px;
-    height: 100px;
    
-} */
-
     
     </style>
   </head>
@@ -61,58 +36,35 @@ width: 100%; background-color: black; height: 100px; margin-top: 1000px;
       <a class="hiddenanchor" id="signin"></a> -->
 <!-- <div class="login_wrapper"> -->
       <div class="login_wrapper" style="min-height: 1200px">
-        <!-- <div class="">
-          <section class="login_content">
+       
+          
+   <section class="login_content">
             <form method="POST" action="create_event.php">
-              <h1>Create Event</h1>              
+              <h1>Assign Leader to Review Committee</h1>              
                 <div>
               <div>
-                Year <br>
+                First Name <br>
                 <input type="text" name="year" class="form-control" placeholder="Event year" required="" />
               </div>
-              <div>Event Name <br>
+              <div>Last Name <br>
                 <input type="text" name="name" class="form-control" placeholder="Name" required="" />
               </div>
-              <div>Chairman <br>
+              <div>Email <br>
                 <input type="text" name="chairman" class="form-control" placeholder="Chairman" required="" />
               </div>
-              <div>Secretary<br>
+              <div>Password<br>
                 <input type="text" name="secretary" class="form-control" placeholder="Secretary" required="" />
               </div>
-              <div>Treasurer <br>
-                <input type="text" name="tresurer" class="form-control" placeholder="Treasurer" required="" />
-              </div>
+              
               
 
               <div>
-                <input type="submit" class="btn btn-success submit" name="submit" value="Create"> 
+                <input type="submit" class="btn btn-success submit" name="submit" value="Create" style="margin-left: 30%; "> 
                   
                 </div>
               </div>
                </form>
             </section>
-              </div>
-         -->   <!-- <div><font size="6"><u>Assign Team Leaders</u></font></div>
- -->
-
-                  <div class="tile-stats">
-                  <div class="icon"><i class="fa fa-book"></i></div>
-                  <div class="count"><a href="leader_assign1.php">Review Committee</a></div>
-                  
-                  
-                </div>
-                <div class="tile-stats">
-                  <div class="icon"><i class="fa fa-book"></i></div>
-                  <div class="count"><a href="leader_assign1.php">Accomodation Committee</a></div>
-                  
-                  
-                </div>
-                <div class="tile-stats">
-                  <div class="icon"><i class="fa fa-book"></i></div>
-                  <div class="count"><a href="leader_assign1.php">Food Committee</a></div>
-                  
-                  
-                </div>
 
   </div>
   
